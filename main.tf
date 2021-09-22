ecf12dfe981ec254e391653090e1439dcf7f8f8e
Dino Serdarević <66571503+LiquidRave@users.noreply.github.com>
2021-09-22 22:50:50 +0200

    first commit for a aws simple deplot

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 3.27"
+      }
+  }
+
+
+required_version = ">= 0.14.9"
+}
+
+
+provider "aws" {
+    profile = "default"
+    region = "eu-central-1"
+}
+
+resource "aws_instance" "ec2-test" {
+    instance_type = "t2.micro"
+    #ubuntu 20.04 
+    ami = "ami-05f7491af5eef733a"
+    tags = {
+        Name = "ec2-test"
+    }
+}
