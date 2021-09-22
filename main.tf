0e2e7de41be809efe8c8018d1c6dfb956c95c8ad
LiquidRave <dino.serdarevic1965@gmail.com>
2021-09-22 23:00:24 +0200

    added two more servers that will serve as web servers for a www cluster

--- main.tf
+++ main.tf
@@ -16,11 +16,28 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_instance" "ec2-test" {
+resource "aws_instance" "haproxy_load_balancer" {
     instance_type = "t2.micro"
     #os ubuntu 20.04 
     ami = "ami-05f7491af5eef733a"
     tags = {
-        Name = "ec2-test"
+        Name = "haproxy_loadbalancer"
+    }
+}
+
+resource "aws_instance" "web-server1" {
+    instance_type = "t2.micro"
+    #os ubuntu 20.04 
+    ami = "ami-05f7491af5eef733a"
+    tags = {
+        Name = "webserver1"
+    }
+}
+resource "aws_instance" "webserver2" {
+    instance_type = "t2.micro"
+    #os ubuntu 20.04 
+    ami = "ami-05f7491af5eef733a"
+    tags = {
+        Name = "webserver2"
     }
 }
