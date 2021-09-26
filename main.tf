69b9f7465486e0fb709a2587fe1826a091ac8316
LiquidRave <dino.serdarevic1965@gmail.com>
2021-09-26 16:39:51 +0200

    added variables and some more IAC

--- main.tf
+++ main.tf
@@ -6,39 +6,100 @@ terraform {
       }
   }
 
-
 required_version = ">= 0.14.9"
 }
-
 
 provider "aws" {
     profile = "default"
     region = "eu-central-1"
 }
 
+resource "aws_budgets_budget" "top_kek" {
+    name = "monthyl-budget"
+    budget_type = "COST"
+    limit_amount = "10.0"
+    limit_unit = "USD"
+    time_unit = "MONTHLY"
+    time_period_start = "2021-09-28_00:01"
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    tags = {
+        Name = "main"
+    }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_route_table" "route_table1" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+}
+resource "aws_security_group" "allow_all" {
+    vpc_id = aws_vpc.main.id
+    description = "allow all"
+    ingress {
+      description      = "open all"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws"
+  public_key = var.pub_key
+}
+
 resource "aws_instance" "haproxy_load_balancer" {
-    instance_type = "t2.micro"
-    #os ubuntu 20.04 
-    ami = "ami-05f7491af5eef733a"
+    instance_type = var.server_type
+    ami = var.ami
+    subnet_id = aws_subnet.main.id
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    key_name = "aws"
     tags = {
         Name = "haproxy_loadbalancer"
     }
 }
 
-resource "aws_instance" "web-server1" {
-    instance_type = "t2.micro"
-    #os ubuntu 20.04 
-    ami = "ami-05f7491af5eef733a"
+resource "aws_instance" "webserver1" {
+    instance_type = var.server_type 
+    ami = var.ami
+    subnet_id = aws_subnet.main.id
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    key_name = "aws"
     tags = {
         Name = "webserver1"
     }
 }
 resource "aws_instance" "webserver2" {
-    instance_type = "t2.micro"
-    #os ubuntu 20.04 
-    ami = "ami-05f7491af5eef733a"
+    instance_type = var.server_type 
+    subnet_id = aws_subnet.main.id
+    ami = var.ami
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    key_name = "aws"
     tags = {
         Name = "webserver2"
     }
 }
-#comment
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "server_type" {
+    description = "define server type"
+    type = string
+    default = "t2.micro"
+}
+
+variable "ami" {
+    description = "os"
+    type = string
+    default = "ami-05f7491af5eef733a"
+}
+
+variable "pub_key" {
+    description = "home pc pub key"
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzR81XbiR3IOW86ZbCI85ZDvlUhmIVIQX20geIdVkTrDa9nCMvGMl4qbmdkTEvQyQeiC2/pl1uKo4k3dz2T0lvPoxxKUS1dS8k8Dc5svIbyX7GQAKuwAGl+Q+CvcGgd8oVO86WjRYD0uxsTAotqiB0k/MKwu5U0U+1TOChPvLIvEfqKsIN8nGXZCovfIdsKtAXRmqJo1gtMJkPZVDAp8kQ7anUTZTamE4/LmgHCgNrB/8zams5rNjMg4PWomJSt0ScoSuWBEa00a6U2nGieN1qSlSEW6A5CKiiauAoQ/OObBxzvCrHs9d73NLJU6ncVlNzeIPoiWaTOWzv38GVAdsabO/Jt0GbmIrLXsv9+Sz7Gw2jgunoU7NvQo5BAuahkTThzZtbhC8W8iLeWipAUE/7GdF96jGsae0rc4fEzAbgcVVL+InqOjmh4E1ocVZbpg0lNs5bDitvfkPx11rht986r26+q/Ji3k7HBysNiadtrsd+G+xEwvCtreqSYEDe7wU= liquid@DESKTOP-LVEV8LN"
+}
